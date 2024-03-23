31f0c688be2fce3c6f25ebfda1d514975f1c984e
nankuF <poltoranin_valeriy@mail.ru>
2024-03-23 09:12:36 +0300

    create project.

---
+++ datafaker/src/test/java/ru/poltoranin/datafaker/DatafakerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.poltoranin.datafaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatafakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
